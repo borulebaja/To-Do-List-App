bb08fb8c6f9edd523aa8bdf2494d711d03010788
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2019-05-24 19:09:16 -0500

    added eventListeners

--- script.js
+++ script.js
@@ -28,3 +28,52 @@ const deleteTodo = todo =>
     storageKey,
     convertObjToString(getTodos().filter(_todo => _todo !== todo))
   );
+
+//to inject to ul
+const appendLiToDOM = element =>
+  document.getElementById("todo-list-container").appendChild(element);
+
+const clearTodoListDisplay = () =>
+  (document.getElementById("todo-list-container").innerHTML = "");
+
+const clearInput = () => (document.getElementById("new-todo-input").value = "");
+
+const displayTodos = () => {
+  clearInput();
+  clearTodoListDisplay();
+  getTodos().forEach(_todo => appendLiToDOM(createTodoElement(_todo)));
+  initClickListeners();
+};
+
+const initClickListeners = () => {
+  Array.from(document.getElementsByClassName("list-group-item")).forEach(
+    _item => {
+      _item.addEventListener("click", $event => {
+        const todo = $event.target.innerText;
+        if (window.confirm("Have you completed this task: " + todo)) {
+          deleteTodo(todo);
+          displayTodos();
+        }
+      });
+    }
+  );
+};
+
+document.addEventListener("DOMContentLoaded", () => displayTodos());
+
+document
+  .getElementById("submit-new-todo-btn")
+  .addEventListener("click", $event => {
+    const newTodoInput = document.getElementById("new-todo-input");
+    if (newTodoInput.value) {
+      addTodo(newTodoInput.value.trim());
+      displayTodos();
+    }
+  });
+
+document
+  .getElementById("reset-storage-btn")
+  .addEventListener("click", $event => {
+    localStorage.removeItem(storageKey);
+    displayTodos();
+  });
